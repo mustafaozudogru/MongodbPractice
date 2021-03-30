063219cd9becb1322cc8ad181da32769cebec57b
mustafa özüdoğru <mustafaozudogru@windowslive.com>
2021-03-30 09:20:47 +0300

    last.

--- MongodbPractice/Controllers/ProductController.cs
+++ MongodbPractice/Controllers/ProductController.cs
@@ -4,8 +4,6 @@ using MongodbPractice.Services;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace MongodbPractice.Controllers
 {
     [Route("api/[controller]")]
